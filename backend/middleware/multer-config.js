466bd4c91d8486287cb96c7aa6ae6fb2ace289ff
steveg1989 <steveg1989@gmail.com>
2022-01-24 15:40:49 -0500

    init

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const storage = multer.diskStorage({
+  // Indicates to multer in which folder to save files
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }, // Tells multer to replace the original name with a timestamp
+});
+// Import of multer and indicates that we only manage downloads of image files
+module.exports = multer({ storage: storage }).single('image');
